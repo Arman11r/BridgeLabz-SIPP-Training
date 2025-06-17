451f5fa5298c9d42c4daebba7447cfdb68068a61
Arman11r <arman0078627@gmail.com>
2025-06-17 21:52:01 +0530

    [Arman Ahmed] add . MultiplicationTable

---
+++ Day3_Arrays/MultiplicationTable.java
@@ -0,0 +1,21 @@
+package Day3_Arrays;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] results = new int[4]; // 6 to 9
+
+        for (int i = 0; i < 4; i++) {
+            results[i] = number * (6 + i);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(number + " * " + (6 + i) + " = " + results[i]);
+        }
+    }
+}
