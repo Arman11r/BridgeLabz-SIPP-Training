91945a9206b19d5111b37b2f725f6f610bb179f2
Arman11r <arman0078627@gmail.com>
2025-06-18 10:11:05 +0530

    [Arman Ahmed] add .ArrayIndexCheck

---
+++ Day5_Methods/ArrayIndexCheck.java
@@ -0,0 +1,17 @@
+package Day5_Methods;
+
+import java.util.*;
+
+public class ArrayIndexCheck {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Scanner sc = new Scanner(System.in);
+        int index = sc.nextInt();
+
+        try {
+            System.out.println("Element at index " + index + " is " + arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Index: " + e.getMessage());
+        }
+    }
+}
