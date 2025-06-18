72b4def35a6333a658f60fb45deea9929c1703c9
Arman11r <arman0078627@gmail.com>
2025-06-18 10:22:08 +0530

    [Arman Ahmed] add .SplitFunction

--- Day5_Strings/SplitFunction.java
+++ Day5_Strings/SplitFunction.java
@@ -6,7 +6,7 @@ public class SplitFunction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String sentence = sc.nextLine();
-        String[] words = sentence.split(" ");
+        String[] words = sentence.split("  ");
 
         for (String word : words) {
             System.out.println(word);
