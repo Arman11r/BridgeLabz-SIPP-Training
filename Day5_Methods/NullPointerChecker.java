0ea9ade81e22a8a6037cf12f72ef9071cc9a97fb
Arman11r <arman0078627@gmail.com>
2025-06-18 10:09:49 +0530

    [Arman Ahmed] add .NullPointerChecker

---
+++ Day5_Methods/NullPointerChecker.java
@@ -0,0 +1,16 @@
+package Day5_Methods;
+
+import java.util.*;
+
+public class NullPointerChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = null;
+
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException");
+        }
+    }
+}
