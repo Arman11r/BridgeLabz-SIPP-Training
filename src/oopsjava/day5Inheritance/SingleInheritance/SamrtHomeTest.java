520f75caf891ab78dd3b7ad4baaca887c68ca9b9
Arman11r <arman0078627@gmail.com>
2025-07-02 19:46:59 +0530

    [Arman Ahmed] add .SmartHomeTest

---
+++ src/oopsjava/day5Inheritance/SingleInheritance/SamrtHomeTest.java
@@ -0,0 +1,38 @@
+package oopsjava.day5Inheritance.SingleInheritance;
+
+class Device {
+    String deviceId;
+    String status;
+
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+class Thermostat extends Device {
+    int temperatureSetting;
+
+    Thermostat(String deviceId, String status, int temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
+public class SamrtHomeTest {
+    public static void main(String[] args) {
+        Thermostat t1 = new Thermostat("THERMO-001", "ON", 24);
+        t1.displayStatus();
+    }
+}
