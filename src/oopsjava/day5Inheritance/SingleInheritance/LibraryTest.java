b585e99c2f8f72d397e78a782ff9ffd40c1df4ef
Arman11r <arman0078627@gmail.com>
2025-07-02 19:45:28 +0530

    [Arman Ahmed] add .LibraryTest

---
+++ src/oopsjava/day5Inheritance/SingleInheritance/LibraryTest.java
@@ -0,0 +1,41 @@
+package oopsjava.day5Inheritance.SingleInheritance;
+
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Bio: " + bio);
+    }
+}
+
+public class LibraryTest {
+    public static void main(String[] args) {
+        Author a1 = new Author("The Time Machine", 1895, "H.G. Wells", "British writer and pioneer of science fiction.");
+        a1.displayInfo();
+    }
+}
