a4d797316247aeb9063db2cf04f46dc7fe06b3a3
Arman11r <arman0078627@gmail.com>
2025-07-14 09:10:37 +0530

    [Arman Ahmed] add .ByteToCharStream

---
+++ src/Day4StringBuilderLinearAndBinarySearch/ByteToCharStream.java
@@ -0,0 +1,15 @@
+package Day4StringBuilderLinearAndBinarySearch;
+
+import java.io.*;
+
+public class ByteToCharStream {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream("sample.txt"), "UTF-8"))) {
+            String line;
+            while ((line = reader.readLine()) != null) System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
